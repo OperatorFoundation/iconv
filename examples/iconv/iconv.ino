7ec22b27853130677fe4afaef42f9eb3e0e8633d
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2021-03-12 11:41:34 -0600

    Better example

--- examples/iconv/iconv.ino
+++ examples/iconv/iconv.ino
@@ -1,6 +1,21 @@
 #include <iconv.h>
 
 void setup() {
+    char src[] = "abcčde";
+    char dst[100];
+    size_t srclen = 6;
+    size_t dstlen = 12;
+ 
+    fprintf(stderr,"in: %s\n",src);
+ 
+    char * pIn = src;
+    char * pOut = ( char*)dst;
+ 
+    iconv_t conv = iconv_open("UTF-8","CP1250");
+    iconv(conv, &pIn, &srclen, &pOut, &dstlen);
+    iconv_close(conv);
+ 
+    fprintf(stderr,"out: %s\n",dst);
 }
 
 void loop() {
